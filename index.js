fe285f21b8bb0df0bc1bfa9e5914baac64ee39f9
SeunDeve <eseun.oyewole@gmail.com>
2022-06-23 13:34:24 +0100

    intro to js

---
+++ index.js
@@ -0,0 +1,10 @@
+const firstName = "Oluwaseun";
+const lastName ="Oyewole";
+
+const height ="184cm";
+const country = "Nigeria"
+
+console.log(firstName,lastName)
+console.log(height)
+console.log(country)
+
